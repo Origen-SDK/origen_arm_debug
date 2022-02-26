6a43062db97453eced4c9cc6410d6c3eee10f572
Paul DeRouen <paul.derouen@nxp.com>
2022-02-25 18:57:13 -0700

    model and controller for dapv6

---
+++ lib/origen_arm_debug/jtag_dp_v6.rb
@@ -0,0 +1,86 @@
+module OrigenARMDebug
+  class JTAG_DPV6
+    include Origen::Model
+
+    attr_reader :dpacc_select, :apacc_select
+
+    def initialize(options = {})
+      options = {
+        ir_size:       4,
+        idcode_select: 0b1110,
+        abort_select:  0b1000,
+        dpacc_select:  0b1010,
+        apacc_select:  0b1011
+      }.merge(options)
+      @dpacc_select = options[:dpacc_select]
+      @apacc_select = options[:apacc_select]
+      add_reg :ir, 0, size: options[:ir_size]
+
+      # Virtual reg used to represent all of the various 35-bit scan chains
+      reg :dr, 0, size: 35 do |reg|
+        reg.bit 34..3, :data
+        reg.bit 2..1, :a
+        reg.bit 0, :rnw
+      end
+
+      reg :idcode, options[:idcode_select], access: :ro do |reg|
+        reg.bit 31..28, :version
+        reg.bit 27..12, :partno
+        reg.bit 11..1, :designer
+        reg.bit 0, :bit0, reset: 1
+      end
+
+      reg :ctrlstat, 0x4, dpbanksel: 0 do |reg|
+        reg.bit 31, :csyspwrupack
+        reg.bit 30, :csyspwrupreq
+        reg.bit 29, :cdbgpwrupack
+        reg.bit 28, :cdbgpwrupreq
+        reg.bit 27, :cdbgrstack
+        reg.bit 26, :cdbgrstreq
+        reg.bit 23..12, :trncnt
+        reg.bit 11..8, :masklane
+        reg.bit 7, :wdataerr
+        reg.bit 6, :readok
+        reg.bit 5, :stickyerr
+        reg.bit 4, :stickycmp
+        reg.bit 3..2, :trnmode
+        reg.bit 1, :stickyorun
+        reg.bit 0, :orundetect
+      end
+
+      reg :select, 0x8 do |reg|
+        reg.bit 31..4, :addr
+        reg.bit 3..0, :dpbanksel
+      end
+
+      reg :select1, 0x4, dpbanksel: 5 do |reg|
+        reg.bit 31..0, :addr
+      end
+
+      select.write options[:dp_select_reset] if options[:dp_select_reset]
+
+      add_reg :rdbuff, 0xC, access: :ro, reset: 0
+
+      reg :abort, options[:abort_select], access: :wo do |reg|
+        reg.bit 0, :dapabort
+      end
+    end
+
+    def select
+      reg(:select)
+    end
+
+    def abort
+      reg(:abort)
+    end
+
+    def is_jtag?
+      true
+    end
+
+    def is_swd?
+      false
+    end
+    alias_method :is_sw?, :is_swd?
+  end
+end
---
+++ lib/origen_arm_debug/jtag_dp_v6_controller.rb
@@ -0,0 +1,168 @@
+module OrigenARMDebug
+  class JTAG_DPControllerV6
+    include Origen::Controller
+    include Helpers
+    include DPControllerV6
+
+    def write_register(reg, options = {})
+      unless reg.writable?
+        fail "The :#{reg.name} register is not writeable!"
+      end
+
+      # DP register write
+      if reg.owner == model
+        # Don't log this one, not really a DP reg and will be included
+        # in the JTAG driver log anyway
+        if reg.name == :ir
+          dut.jtag.write_ir(reg)
+        else
+
+          log "Write JTAG-DP register #{reg.name.to_s.upcase}: #{reg.data.to_hex}" do
+            if reg.name == :abort
+              ir.write!(reg.offset)
+              dr.reset
+              dr.overlay(nil)
+              dr[2..0].write(0)
+              dr[34..3].copy_all(reg)
+              dut.jtag.write_dr(dr)
+
+            # DPACC
+            elsif reg.name == :select
+              dp_write(reg)
+
+            # Some other debug register
+            elsif reg.meta.include?(:dpbanksel)
+              if model.reg(:select).bits(:dpbanksel) != reg.meta(:dpbanksel)
+                model.reg(:select).bits(:dpbanksel).write! reg.meta(:dpbanksel)
+              end
+              dp_write(reg)
+
+            else
+              fail "Can't write #{reg.name}"
+            end
+          end
+        end
+
+      # AP register write
+      else
+
+        unless reg.owner.is_a?(AP)
+          fail 'The JTAG-DP can only write to DP or AP registers!'
+        end
+
+        select_ap_reg(reg)
+        dr.reset
+        dr.overlay(nil)
+        dr[0].write(0)
+        dr[2..1].write(reg.offset >> 2)
+        dr[34..3].copy_all(reg)
+        ir.write!(apacc_select)
+        dut.jtag.write_dr(dr, options)
+      end
+    end
+
+    def read_register(reg, options = {})
+      unless reg.readable?
+        fail "The :#{reg.name} register is not readable!"
+      end
+
+      if reg.owner == model
+        # Don't log this one, not really a DP reg and will be included
+        # in the JTAG driver log anyway
+        if reg.name == :ir
+          dut.jtag.read_ir(reg)
+        else
+
+          log "Read JTAG-DP register #{reg.name.to_s.upcase}: #{Origen::Utility.read_hex(reg)}" do
+            if reg.name == :idcode
+              ir.write!(reg.offset)
+              dut.jtag.read_dr(reg)
+
+            # DPACC
+            elsif reg.name == :select || reg.name == :rdbuff
+              dp_read(reg, options)
+
+            # Some other register
+            elsif reg.meta.include?(:dpbanksel)
+              # Part 1 - Set dpbanksel if required
+              if model.reg(:select).bits(:dpbanksel).data != reg.meta(:dpbanksel)
+                model.reg(:select).bits(:dpbanksel).write! reg.meta(:dpbanksel)
+              end
+
+              dp_read(reg, options)
+
+            else
+              fail "Can't read #{reg.name}"
+            end
+          end
+        end
+
+      # AP register read
+      else
+        unless reg.owner.is_a?(AP)
+          fail 'The JTAG-DP can only write to DP or AP registers!'
+        end
+
+        # Part 1 - Request read from AP-Register by writing to APACC with RnW=1
+        select_ap_reg(reg)
+        dr.reset
+        dr.overlay(nil)
+        dr[0].write(1)
+        dr[2..1].write(reg.offset >> 2)
+        dr[34..3].write(0)
+        ir.write!(apacc_select)
+        dut.jtag.write_dr(dr)
+
+        # Calling AP should provide any delay parameter for wait states between AP read request
+        #   and when the data is available at the RDBUFF DP-Reg
+        if options[:apacc_wait_states]
+          options[:apacc_wait_states].cycles
+        end
+
+        # Part 2 - Now read real data from RDBUFF (DP-Reg)
+        dr.reset
+        dr.overlay(nil)
+        dr[0].write(1)
+        dr[2..1].write(rdbuff.offset >> 2)
+        dr[34..3].copy_all(reg)
+        options[:mask] = options[:mask] << 3 unless options[:mask].nil?
+        ir.write!(dpacc_select)
+        dut.jtag.read_dr(dr, options)
+      end
+    end
+
+    def dp_read(reg, options = {})
+      # Part 1 - Request read from DP-Register by writing to DPACC with RnW=1
+      dr.reset
+      dr.overlay(nil)
+      dr[0].write(1)
+      dr[2..1].write(reg.offset >> 2)
+      dr[34..3].write(0)
+      ir.write!(dpacc_select)
+      dut.jtag.write_dr(dr)
+
+      # Part 2 - Now read real data from RDBUFF (DP-Reg)
+      dr.reset
+      dr.overlay(nil)
+      dr[0].write(1)
+      dr[2..1].write(rdbuff.offset >> 2)
+      dr[34..3].copy_all(reg)
+      options[:mask] = options[:mask] << 3 unless options[:mask].nil?
+      dut.jtag.read_dr(dr, options)
+    end
+
+    def dp_write(reg)
+      dr.reset
+      dr.overlay(nil)
+      dr[0].write(0)
+      dr[2..1].write(reg.offset >> 2)
+      dr[34..3].copy_all(reg)
+      ir.write!(dpacc_select)
+      dut.jtag.write_dr(dr)
+    end
+
+    def base_address
+      model.base_address
+    end
+  end
+end
